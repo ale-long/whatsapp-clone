0aebb5b9c7e2753d22de5e15f84a39493ad9a4ac
Alex <alelong@ucdavis.edu>
2021-03-27 20:33:59 -0700

    added top section of sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,18 +2,25 @@ import React from 'react'
 import './Sidebar.css';
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import {Avatar, IconButton} from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-
+                <Avatar src=""/>
                 <div className="sidebar__headerRight">
                     <IconButton>
                         <DonutLargeIcon/>
                     </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
                 </div>
-                <div className="sidebar__headerLeft"></div>
             </div>
         </div>
     )
