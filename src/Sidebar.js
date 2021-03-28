d69b1e2c76eba15b1ed9d0e11bef515267c35bdb
Alex <alelong@ucdavis.edu>
2021-03-27 18:32:27 -0700

    general page outline

---
+++ src/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Sidebar.css';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import {Avatar, IconButton} from "@material-ui/core";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                </div>
+                <div className="sidebar__headerLeft"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
